b8cbc0c18ff8a51b8b0ad104ab778b43b77ec57a
Jordan <jminjie@umich.edu>
2016-10-24 23:30:11 -0700

    Add Finish button animation, omsdroid and gps-loc

--- app/src/main/java/com/jminjie/noober/MainActivity.java
+++ app/src/main/java/com/jminjie/noober/MainActivity.java
@@ -8,12 +8,7 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.Slide;
-import android.transition.Transition;
-import android.transition.TransitionManager;
-import android.transition.TransitionSet;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -25,13 +20,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Button mRequestDriverButton, mRequestRiderButton, mFinishButton;
@@ -41,7 +43,11 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue mRequestQueue;
     private IMapController mMapController;
     final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+    final int PERMISSIONS_REQUEST_MULTIPLE = 2;
     final String TAG = "MainActivity";
+
+    private MyLocationNewOverlay mLocationOverlay;
 
     private Animation mSlideLeftAnimation, mSlideRightAnimation = null;
 
@@ -66,17 +72,24 @@ public class MainActivity extends AppCompatActivity {
                 R.anim.slide_right_animation);
 
         // set up osm
-        // TODO check for permission WRITE_EXTERNAL_STORAGE at runtime
         org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
         mMapView = (MapView) findViewById(R.id.map);
         mMapView.setTileSource(TileSourceFactory.MAPNIK);
         mMapView.setBuiltInZoomControls(true);
         mMapView.setMultiTouchControls(true);
         mMapController = mMapView.getController();
-        mMapController.setZoom(9);
+        mMapController.setZoom(17);
         GeoPoint startPoint = new GeoPoint(30.0, -100.0);
         mMapController.setCenter(startPoint);
+        mMapView.setTilesScaledToDpi(true);
 
+        this.mLocationOverlay = new MyLocationNewOverlay(
+                new GpsMyLocationProvider(getApplicationContext()), mMapView);
+        this.mLocationOverlay.enableMyLocation();
+        mMapView.getOverlays().add(this.mLocationOverlay);
+
+        // Get permissions
+        promptUserForPermissions();
     }
 
     /**
@@ -98,15 +111,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Define the behavior upon receiving a successfull response from the server as a String
+     * Define the behavior upon receiving a successful response to a rider requesting driver
+     * from the server as a JSONObject
      */
-    private Response.Listener<String> responseListener = new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            // Display the first 20 characters of the response string.
-            mTopText.setText("Response is: " + response.substring(0, 20));
-        }
-    };
+    private Response.Listener<JSONObject> driverResponseListener =
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    // Display the first 20 characters of the response string.
+                    mTopText.setText(response.toString());
+                }
+            };
+
+    /**
+     * Define the behavior upon receiving a successful response to driver requesting rider
+     * from the server as a JSONObject
+     */
+    private Response.Listener<JSONObject> riderResponseListener =
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d(TAG, "onResponse called");
+                    mTopText.setText(response.toString());
+                    // Show the returned rider's location on the map
+                    try {
+                        GeoPoint riderGeoPoint = new GeoPoint(response.getDouble("lat"), response.getDouble("lon"));
+                        final OverlayItem riderLocationOverlayItem = new OverlayItem("", "", riderGeoPoint);
+                        ArrayList<OverlayItem> overlayItemList = new ArrayList<>();
+                        overlayItemList.add(riderLocationOverlayItem);
+                        ItemizedIconOverlay<OverlayItem> myOverlay =
+                                new ItemizedIconOverlay<>(overlayItemList,
+                                        null, getApplicationContext());
+
+                        mMapView.getOverlays().add(myOverlay);
+                        mMapController.animateTo(riderGeoPoint);
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                        mTopText.setText("Exception in riderResponseListener");
+                    }
+                }
+            };
 
     /**
      * Define the behavior upon receiving an error from the server
@@ -114,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
     private Response.ErrorListener responseErrorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
-            mTopText.setText("That didn't work!");
+            mTopText.setText("Error from server");
         }
     };
 
@@ -126,43 +170,41 @@ public class MainActivity extends AppCompatActivity {
      * @param lon = the user's longitude
      */
     private void sendDriverOrRiderRequest(String driverOrRider, Double lat, Double lon) {
-        // form the url as "server/<driver_or_rider>?coords=<lat,long>
+        // form the url as "server/<driver_or_rider>?coords=<lat,lon>
         String coords = lat.toString() + "," + lon.toString();
         String url = "http://jminjie.com:5000/noober/" + driverOrRider + "?coords=" + coords;
         Log.d(TAG, "Sent GET to " + url);
 
         // create the request
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                responseListener, responseErrorListener);
+        JsonObjectRequest stringRequest = null;
+        if (driverOrRider.equals("driver")) {
+            stringRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    driverResponseListener, responseErrorListener);
+        } else if (driverOrRider.equals("rider")) {
+            stringRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    riderResponseListener, responseErrorListener);
+        }
 
         // add the request to the RequestQueue to be sent automatically
         mRequestQueue.add(stringRequest);
     }
 
     /**
-     * Get the best location I can
-     * TODO replace this with google_location_services
+     * Get the best location available
      * @return
      */
     private Location getBestLocation() {
-        LocationManager locationManager =
-                (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        // TODO respond to location updates
-        // TODO e.g.) https://developer.android.com/guide/topics/location/strategies.html
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Enable runtime permission request and callback method on request granted
-            // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-            //         PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return null;
+        Location bestLocation = mLocationOverlay.getLastFix();
+        if (bestLocation == null) {
+            LocationManager locationManager =
+                    (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return null;
+            }
+            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
-        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        return bestLocation;
     }
 
 
@@ -184,17 +226,18 @@ public class MainActivity extends AppCompatActivity {
         // Send request for a driver
         Location userLocation = getBestLocation();
         if (userLocation != null) {
+            // show the user overlay
             double currentLatitude = userLocation.getLatitude();
             double currentLongitude = userLocation.getLongitude();
+            Log.d(TAG, "onRiderRequestDriverTap animating to current loc: " + currentLatitude + " " + currentLongitude);
             GeoPoint currentGeoPoint = new GeoPoint(currentLatitude, currentLongitude);
-            mMapController.setCenter(currentGeoPoint);
-            mMapController.setZoom(20);
+            mMapController.setZoom(23);
+            mMapController.animateTo(currentGeoPoint);
             sendDriverOrRiderRequest("driver", currentLatitude, currentLongitude);
         }
     }
 
     /**
-     * TODO could probably merge this function with onRiderRequestDriverTap(...)
      * This function should be called if a driver is requesting a rider by tapping the button
      * Sends the user's location to server/rider
      * @param v
@@ -214,9 +257,6 @@ public class MainActivity extends AppCompatActivity {
         if (userLocation != null) {
             double currentLatitude = userLocation.getLatitude();
             double currentLongitude = userLocation.getLongitude();
-            GeoPoint currentGeoPoint = new GeoPoint(currentLatitude, currentLongitude);
-            mMapController.setCenter(currentGeoPoint);
-            mMapController.setZoom(20);
             sendDriverOrRiderRequest("rider", currentLatitude, currentLongitude);
         }
     }
@@ -247,5 +287,65 @@ public class MainActivity extends AppCompatActivity {
     public void onFinishTap(View v) {
         enableRequestButtons();
         hideFinishButton();
+    }
+
+    private boolean accessFineLocationGranted() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean writeExternalStorageGranted() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * Prompt user to allow the permissions that we need to receive at runtime
+     */
+    private void promptUserForPermissions() {
+        if (!accessFineLocationGranted() && !writeExternalStorageGranted()) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, PERMISSIONS_REQUEST_MULTIPLE);
+        }
+        if (!accessFineLocationGranted()) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        if (!writeExternalStorageGranted()) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    /**
+     * Handle the permissions request response. If the permission is granted, continue running the
+     * app. Otherwise, close the app immediately.
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE:
+                // intentional fall through
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length == 0)
+                    finish();
+                return;
+            }
+            case PERMISSIONS_REQUEST_MULTIPLE: {
+                if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    return;
+                } else {
+                    finish();
+                }
+            }
+        }
     }
 }
